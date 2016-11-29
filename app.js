564ac41c47e4902aec7620d4205537d083015d2c
Jay Balderas <rouesatyr@gmail.com>
2016-11-29 14:45:45 -0800

    Finished question 1 and made the text blue

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-alert('I AM ALIVE!!');
+'use strict';
+
+// The questions in the guessing game must require a mix of yes/no answers, and user input must accept either y/n or yes/no responses, with either .toUpperCase() or .toLowerCase() used to validate the user input and accommodate users entering all-caps Y/N or YES/NO answers, too.
+
+var question1 = prompt('Does Sam have any cats?').toLowerCase();
+
+// if (condition is true) {
+//   do something;
+//   maybe do something more;
+//   etc. until done;
+// } else {
+//   do this other thing;
+//   and maybe yet another thing;
+// }
+
+if (question1 === 'yes' || question1 ==='y' || question1 === 'hell yeah'); {
+  alert('Yes, Sam does have three cats!');
+} else if (question1 === 'no' || question1 ==='n'); {
+  alert('Sorry, you\'re wrong, Sam has three cats');
+} else {
+  alert('Use either \"yes\" or \"no\"');
+}
