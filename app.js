e0c3e5bfad1f72b04bc8be699c5ea9c42c3ec045
Siamak Broomand <broomand.siamk85@gmail.com>
2016-12-01 15:19:52 -0800

    added functions

--- app.js
+++ app.js
@@ -15,125 +15,141 @@
 var counterNumbers = 0;
 var response1 = prompt('Does Sam have any cats?').toLowerCase();
 
+function question1(){
+  if (response1 === 'yes' || response1 ==='y') {
+    alert('Yes, Sam does have three cats!');
 
-if (response1 === 'yes' || response1 ==='y') {
-  alert('Yes, Sam does have three cats!');
+    counterNumbers++;
 
-  counterNumbers++;
+  } else if (response1 === 'no' || response1 ==='n') {
+    alert('Sorry, you\'re wrong, Sam has three cats');
+  } else {
+    alert('Use either \"yes\" or \"no\"');
+  }
 
-} else if (response1 === 'no' || response1 ==='n') {
-  alert('Sorry, you\'re wrong, Sam has three cats');
-} else {
-  alert('Use either \"yes\" or \"no\"');
+  console.log('The user selected ' + response1 + ' to Sam having cats.');
 }
+question1();
 
-console.log('The user selected ' + response1 + ' to Sam having cats.');
+function question2(){
+  var response2 = prompt('Should you learn how to code efficiently?').toLowerCase();
 
-var response2 = prompt('Should you learn how to code efficiently?').toLowerCase();
+  if (response2 === 'yes' || response2 ==='y') {
+    alert('Yes, it\'s the only way to advance in your career!');
 
-if (response2 === 'yes' || response2 ==='y') {
-  alert('Yes, it\'s the only way to advance in your career!');
+    counterNumbers++;
 
-  counterNumbers++;
+  } else if (response2 === 'no' || response2 ==='n') {
+    alert('Sorry, you won\'t get to far if you don\'t.');
+  } else {
+    alert('Use either \"yes\" or \"no\"');
+  }
 
-} else if (response2 === 'no' || response2 ==='n') {
-  alert('Sorry, you won\'t get to far if you don\'t.');
-} else {
-  alert('Use either \"yes\" or \"no\"');
+  console.log('The user selected ' + response2 + ' to the question.');
 }
+question2();
 
-console.log('The user selected ' + response2 + ' to the question.');
+function question3(){
+  var response3 = prompt('Does this prompt require a console.log message to appear?').toLowerCase();
 
-var response3 = prompt('Does this prompt require a console.log message to appear?').toLowerCase();
+  if (response3 === 'yes' || response3 ==='y') {
+    alert('Yes, because that\'s what the instructions said.');
 
-if (response3 === 'yes' || response3 ==='y') {
-  alert('Yes, because that\'s what the instructions said.');
+    counterNumbers++;
 
-  counterNumbers++;
+  } else if (response3 === 'no' || response3 ==='n') {
+    alert('Sorry, that would only work if you\'re not trying to get all of the points');
+  } else {
+    alert('Use either \"yes\" or \"no\"');
+  }
 
-} else if (response3 === 'no' || response3 ==='n') {
-  alert('Sorry, that would only work if you\'re not trying to get all of the points');
-} else {
-  alert('Use either \"yes\" or \"no\"');
+  console.log('The user selected ' + response3 + ' to adding a console.log');
 }
+question3();
 
-console.log('The user selected ' + response3 + ' to adding a console.log');
+function question4(){
+  var response4 = prompt('Are you allowed to use functions today??').toLowerCase();
 
-var response4 = prompt('Are you allowed to use functions today??').toLowerCase();
+  if (response4 === 'yes' || response1 ==='y') {
+    alert('Sorry, no, not today, until thursday.');
+  } else if (response4 === 'no' || response4 ==='n') {
+    alert('You\'re right, stick to the main goal today!');
 
-if (response4 === 'yes' || response1 ==='y') {
-  alert('Sorry, no, not today, until thursday.');
-} else if (response4 === 'no' || response4 ==='n') {
-  alert('You\'re right, stick to the main goal today!');
+    counterNumbers++;
 
-  counterNumbers++;
+  } else {
+    alert('Use either \"yes\" or \"no\"');
+  }
 
-} else {
-  alert('Use either \"yes\" or \"no\"');
+  console.log('The user said ' + response4 + ' to using functions today');
 }
+question4();
 
-console.log('The user said ' + response4 + ' to using functions today');
+function question5(){
+  var response5 = prompt('Are you supposed to rush through the lab today?').toLowerCase();
 
-var response5 = prompt('Are you supposed to rush through the lab today?').toLowerCase();
+  if (response5 === 'yes' || response5 ==='y') {
+    alert('No, it\'s better to take your time to learn this stuff.');
+  } else if (response5 === 'no' || response5 ==='n') {
+    alert('Good, you\'ll be a badass coder soon!');
 
-if (response5 === 'yes' || response5 ==='y') {
-  alert('No, it\'s better to take your time to learn this stuff.');
-} else if (response5 === 'no' || response5 ==='n') {
-  alert('Good, you\'ll be a badass coder soon!');
+    counterNumbers++;
 
-  counterNumbers++;
+  } else {
+    alert('Use either \"yes\" or \"no\"');
+  }
 
-} else {
-  alert('Use either \"yes\" or \"no\"');
+  console.log('The user selected ' + response5 + ' to rushing the lab today.')
 }
-
-console.log('The user selected ' + response5 + ' to rushing the lab today.')
-
+question5();
 // As a developer, I want to add a sixth question to my guessing game that takes numeric input by prompting a user to guess a number (for instance, "What is my favorite number?" or "How many Pokémon did I catch yesterday?"), and indicates to the user whether the guess is 'too high' or 'too low', and gives the user exactly four opportunities to get the correct answer, so that my fancy programming skills are showcased.
+function question6(){
+  var age = 26;
 
-var age = 26;
+  while (age !== 26);
+  for (var i = 0; i < 4; i++) {
+    var yourGuess = (parseInt(prompt('What is your guess for Jay\'s age?')));
 
-while (age !== 26);
-for (var i = 0; i < 4; i++) {
-  var yourGuess = (parseInt(prompt('What is your guess for Jay\'s age?')));
-
-  if (yourGuess === age) {
-    alert('You got it right!! Jay is' + age + '!');
-    break;
-  }
-  else if (yourGuess > 26) {
-    alert('Try to guess lower!');
-  }
-  else if (yourGuess < 26) {
-    alert('Try to guess higher!')
-  }
-}
-
-var placesLived = ['denver', 'seattle', 'miami', 'chicago', 'los angeles', 'san diego', 'honolulu'];
-var counter = 0;
-var gotCityRight = false;
-while (counter < 6){
-
-  var yourCityGuess = prompt('Guess a city that Jay has lived in? You have 6 tries!').toLowerCase();
-
-  for (var j = 0; j < placesLived.length; j++) {
-    console.log(yourCityGuess);
-
-    if(yourCityGuess === placesLived[j]) {
-      gotCityRight = true;
+    if (yourGuess === age) {
+      alert('You got it right!! Jay is' + age + '!');
       break;
     }
+    else if (yourGuess > 26) {
+      alert('Try to guess lower!');
+    }
+    else if (yourGuess < 26) {
+      alert('Try to guess higher!')
+    }
   }
-  if(gotCityRight === true) {
-    alert ('yes! you gussed ' + yourCityGuess + ', which is one of the places Jay has lived!!');
-    alert ('He has lived in denver, seattle, miami, chicago, los angeles, san diego, and honolulu!');
-    break;
-  } else{
-    alert ('Jay has never lived in ' + yourCityGuess + ' try again!');
-  }
-  console.log(counter);
-  counter+= 1;
 }
+question6();
 
+function question7(){
+  var placesLived = ['denver', 'seattle', 'miami', 'chicago', 'los angeles', 'san diego', 'honolulu'];
+  var counter = 0;
+  var gotCityRight = false;
+  while (counter < 6){
+    var yourCityGuess = prompt('Guess a city that Jay has lived in? You have 6 tries!').toLowerCase();
+
+    for (var j = 0; j < placesLived.length; j++) {
+      console.log(yourCityGuess);
+
+      if(yourCityGuess === placesLived[j]) {
+        gotCityRight = true;
+        break;
+      }
+    }
+    if(gotCityRight === true) {
+      alert ('yes! you gussed ' + yourCityGuess + ', which is one of the places Jay has lived!!');
+      alert ('He has lived in denver, seattle, miami, chicago, los angeles, san diego, and honolulu!');
+      break;
+    } else{
+      alert ('Jay has never lived in ' + yourCityGuess + ' try again!');
+    }
+    console.log(counter);
+    counter+= 1;
+  }
+}
+question7();
 counterNumbers = alert('nice job you got ' + counterNumbers + ' out of 7!');
 console.log('User got ' + counterNumbers + ' questions right');
